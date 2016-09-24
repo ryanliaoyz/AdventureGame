2b33dcafe81ac90d9bc83f1b60f4e827907afe9d
ryanliaoyz <liaoyunze@hotmail.com>
2016-09-24 08:47:25 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def cave():
+	print("You're in a cave, you may die, Do you wnat to go left or right?")
+	answer.cave = input("What is your choice?")
+	answer = answer.lower()
+	if answer = "left" or "l":
+		room_2()
+	elif answer = "right" or "r":
+		room_3()
+
