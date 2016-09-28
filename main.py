bf4c9ed8b7d367e0eabc3214da7757cf023f8cdc
ryanliaoyz <liaoyunze@hotmail.com>
2016-09-28 19:44:51 -0400

    beta 1

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def intro():
+	print("  ___ ___                    /\             ____.                                            _____       .___                    __                        ")
+	print(" /   |   \   ___________  ___)/ ______     |    | ____  __ _________  ____   ____ ___.__.   /  _  \    __| _/__  __ ____   _____/  |_ __ _________   ____  ")
+	print("/    ~    \_/ __ \_  __ \/  _ \/  ___/     |    |/  _ \|  |  \_  __ \/    \_/ __ <   |  |  /  /_\  \  / __ |\  \/ // __ \ /    \   __\  |  \_  __ \_/ __ \ ")
+	print("\    Y    /\  ___/|  | \(  <_> )___ \  /\__|    (  <_> )  |  /|  | \/   |  \  ___/\___  | /    |    \/ /_/ | \   /\  ___/|   |  \  | |  |  /|  | \/\  ___/ ")
+	print(" \___|_  /  \___  >__|   \____/____  > \________|\____/|____/ |__|  |___|  /\___  > ____| \____|__  /\____ |  \_/  \___  >___|  /__| |____/ |__|    \___  >")
+	print("       \/       \/                 \/                                    \/     \/\/              \/      \/           \/     \/                        \/ ")
 	print("You are a freshman in South Kent School, you missed the lunch and about miss the CFI in North Campus!")
+	input("press enter to continue...")
 	hill()
 
 def hill():
@@ -19,6 +26,7 @@ def halfway():
 	answer = answer.lower()
 	if answer == "yes":
 		print("you crashed by the car when chasing, you fully recovered after 6 month, and you missed the bus to CFI again!")
+		input("press enter to continue...")
 		hill()
 	elif answer == "no":
 		dorm()
@@ -31,6 +39,7 @@ def dorm():
 	answer = answer.lower()
 	if answer == "yes":
 		print("Mr. Darrin have a conversation with you, you could remain in school but can’t have any class absence. Sadly, you missed the bus to CFI again.")
+		input("press enter to continue...")
 		hill()		
 	elif answer == "no":
 		figure()
@@ -45,6 +54,7 @@ def figure():
 		bear()
 	elif answer == "no":
 		print("you open the Uber app, it shows “no cars available”")
+		input("press enter to continue...")
 		figure()
 	else:
 		figure()
@@ -59,12 +69,15 @@ def bear():
 		answerw = answerw.lower()
 		if answerw == "stick" or answerw == "stone":
 			print("you wounded, by the dumb decision of fighting with a bear with a ", answerw, " and you fully recovered after 6 month, and you missed the bus to CFI again.")
+			input("press enter to continue...")
 			hill()
 		else:
 			print("how could you have a ", answerw,"in mid of the forest?")
+			input("press enter to continue...")
 			bear()
 	elif answer == "no":
 		print("The bear doesn’t chase you, you make alive safely from the bear.")
+		input("press enter to continue...")
 		noise()
 	else:
 		bear()
@@ -79,15 +92,19 @@ def noise():
 		answerw = answerw.lower()
 		if answerw == "yes":
 			print("Mr. Mittag didn't notice you, and crush your leg, after you fully recovered, you miss the bus again.")
+			input("press enter to continue...")
 			hill()
 		elif answerw == "no":
 			print("the car drove away.")
+			input("press enter to continue...")
 			walk()
 		else:
 			print(answerw, "is not a good option, you need a better one.")
+			input("press enter to continue...")
 			noise()
 	elif answer == "no":
 		print("it is Mr. Mittag's SUV, he's happy to drive you to CFI.")
+		input("press enter to continue...")
 		drive()
 	else:
 		noise()
@@ -99,15 +116,20 @@ def walk():
 	if answer == "yes":
 		print("You twist your ankle by a rock, and tumble down 50 feet into ditch.")
 		print(". \n. \n. ")
+		input("press enter to continue...")
 		print("Mr. Mittag rescue you after 2 hours, you fully recovered in 3 months")
 		print(". \n. \n. ")
+		input("press enter to continue...")
 		print("You missed your bus to North Campus again!")
 		print(". \n. \n. ")
+		input("press enter to continue...")
 		hill()
 	elif answer == "no":
 		print("the hawl dived to you and bite your ear down, Mr. Mittag hear your screaming, so he turn back and pick you up to hospital.")
 		print(". \n. \n. ")
+		input("press enter to continue...")
 		print("After you out of hospital, you missed the CFI again")
+		input("press enter to continue...")
 		hill()
 	else:
 		walk()
@@ -115,8 +137,10 @@ def walk():
 def drive():
 	print("Mr. Mittag's SUV tire has been punctured by a nail left by someone(abosuletly not Ryan)You guys need to push the car to North Campus.")
 	print(". \n. \n. ")
+	input("press enter to continue...")
 	print("When you guys arrive, the CFI bus just drove back to main campus.")
 	print(". \n. \n. ")
+	input("press enter to continue...")
 	tryagain()
 
 def tryagain():
